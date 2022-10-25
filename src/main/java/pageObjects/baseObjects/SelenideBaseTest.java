581221212fb05b085e3eb1cc95fc7a8458bd3308
MariyaKc <111488221+MariyaKc@users.noreply.github.com>
2022-10-25 15:56:28 +0300

    lesson 17

---
+++ src/main/java/pageObjects/baseObjects/SelenideBaseTest.java
@@ -0,0 +1,24 @@
+package pageObjects.baseObjects;
+
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.Listeners;
+import testNgUtils.SelenideListener;
+
+import java.util.Properties;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.page;
+import static com.codeborne.selenide.WebDriverRunner.driver;
+import static propertyHelper.PropertyReader.getProperties;
+
+/**реализует метод get, который стартует драйвер, если его нет и создает инстанс страницы */
+@Listeners(SelenideListener.class)
+public class SelenideBaseTest {
+
+    protected <T> T get(Class<T> page) {
+        return driver().hasWebDriverStarted() ? page(page) : open(Configuration.baseUrl, page);
+    }
+// page не создает драйвер, но возвращает инстанс
+//open создает драйвер и возвращает инстанс
+//hasWebDriverStarted() спрашивает состояние драйвера
+}
